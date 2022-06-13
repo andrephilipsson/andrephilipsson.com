72e1be7e62974cb375cb4800f953a953d5a4f353
André Philipsson <andre@andrephilipsson.com>
2022-06-13 22:01:12 +0200

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import styles from "./styles/tailwind.css"
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: styles }]
+}
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="min-h-screen">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <main className="flex justify-center items-center min-h-screen bg-zinc-100 dark:bg-zinc-900">
+      <div className="text-center text-zinc-900 dark:text-zinc-200">
+        <h1 className="font-semibold text-2xl">Hello!</h1>
+        <a href="mailto:hello@andrephilipsson.com" className="underline underline-offset-2 text-sm">Contact me</a>
+      </div>
+    </main>
+  );
+}
